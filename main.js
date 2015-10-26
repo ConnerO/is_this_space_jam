5a21d60609123acf96b41a878a3cf45558d42c72
Kyle Smith <tman2kyle@hotmail.com>
2015-10-26 13:57:41 -0500

    mvp

---
+++ main.js
@@ -0,0 +1,22 @@
+var array = ["No it's not Space Jam", 
+"No it's not The Jam",
+"Still not Space Jam",
+"You wish this was Space Jam",
+"If you think this movie is Space Jam, you clearly don't know Space Jam",
+"This isn't Space Jam, therefore you are stupid"];
+console.log("Alert are lames");
+
+
+$("#submitter").click(function(){
+    var userInput = $("#SpaceJam-checker").val();
+    if (userInput === "Space Jam")
+    {
+        console.log("This is Space Jam");
+    }
+    else
+    {
+        var randomIndex =
+        console.log(array[randomIndex]);
+    }
+});
+
