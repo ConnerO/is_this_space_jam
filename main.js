e79cf8b074defa712a3f4e0fa317f67e2b210306
Kyle Smith <tman2kyle@hotmail.com>
2015-10-26 19:49:52 -0500

    punch the keys

--- main.js
+++ main.js
@@ -39,19 +39,23 @@ $("#submitter").click(function(){
           oldMessages = [];
           doEverything();
         }
-
       }
     }
-
   }
 });
 
-FB.getLoginStatus(function(response) {
-  if (response.status === 'connected') {
-    console.log('Logged in.');
-  }
-  else {
-    FB.login();
-  }
-});
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : 'your-app-id',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
 
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
