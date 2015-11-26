5d9f0d97e778f1bb00225222039c0cafcfb40a4f
kyle smith <chessmonger2112@yahoo.com>
2015-11-26 10:52:23 -0800

    debugging...

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,7 +7,7 @@ var array = ["No it's not Space Jam",
 "If you think this movie is Space Jam, you clearly don't know Space Jam",
 "This isn't Space Jam, therefore you are stupid"];
 console.log("Alert are lames");
-testResponse;
+// testResponse = 0;
 
 $("#submitter").click(function(){
   var userInput = $("#SpaceJam-checker").val();
