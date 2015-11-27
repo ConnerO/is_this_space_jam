83e54862ea6c5e3e7987f88b54851dcc0b86ae05
kyle smith <chessmonger2112@yahoo.com>
2015-11-26 19:22:37 -0800

    add a console log at the beginning to see if it shows in heroku

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,6 +6,7 @@ var array = ["No it's not Space Jam",
 "If you think this movie is Space Jam, you clearly don't know Space Jam",
 "This isn't Space Jam, therefore you are stupid"];
 console.log("Alert are lames");
+console.log("This isn't going to show is it.");
 name = "";
 // testResponse = 0;
 
