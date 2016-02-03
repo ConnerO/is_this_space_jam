5ba4c48a3af97698064a8f72ef63c69000601d03
kyle smith <chessmonger2112@yahoo.com>
2016-02-03 14:43:33 -0800

    adding publish action permission

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -108,6 +108,9 @@ function myFacebookLogin()
 
 function checkLoginState()
 {
+  FB.login(function(response) {
+  // handle the response
+}, {scope: 'email,user_likes'});
   FB.getLoginStatus(function(response)
   {
     checkLogResponse = response;
