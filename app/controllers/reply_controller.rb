d10c7c5d8ae0969fa751a9525b30e4affaa95ff0
kyle smith <chessmonger2112@yahoo.com>
2016-06-02 14:31:05 -0700

    commented everything out except the replying json part

--- app/controllers/reply_controller.rb
+++ app/controllers/reply_controller.rb
@@ -1,11 +1,12 @@
 class ReplyController < ApplicationController
   def reply
-    puts "wake up, reply. Stand up, reply"
-    replies = Reply.all
-    replies_filtered = []
-    replies.each do |reply|
-      replies_filtered << reply.reply
-    end
-    render :json => {poo:replies_filtered}
+    # puts "wake up, reply. Stand up, reply"
+    # replies = Reply.all
+    # replies_filtered = []
+    # replies.each do |reply|
+    #   replies_filtered << reply.reply
+    # end
+    # render :json => {poo:replies_filtered}
+    render :json => {sup: "supppppppp"}
   end
 end
