36ef0624e77cdf05e157428ab3c68aa2b4815e27
kyle smith <chessmonger2112@yahoo.com>
2016-06-02 14:39:46 -0700

    added a new puts

--- app/controllers/reply_controller.rb
+++ app/controllers/reply_controller.rb
@@ -2,11 +2,12 @@ class ReplyController < ApplicationController
   def reply
     puts "Shake and Bake!!!"
     replies = Reply.all
+    puts replies
     replies_filtered = []
     # replies.each do |reply|
     #   replies_filtered << reply.reply
     # end
     # render :json => {poo:replies_filtered}
-    render :json => {dong: "dongssssssss"}
+    render :json => {dong: "hceking what replies does"}
   end
 end
