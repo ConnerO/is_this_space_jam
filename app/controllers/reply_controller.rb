eb713a8e3b0ab62dd7b8774d9ee1bb54c3cab14e
kyle smith <chessmonger2112@yahoo.com>
2016-06-02 14:27:34 -0700

    move puts to the top before potential error in reply action in reply controller.

--- app/controllers/reply_controller.rb
+++ app/controllers/reply_controller.rb
@@ -1,11 +1,11 @@
 class ReplyController < ApplicationController
   def reply
+    puts "wake up, reply. Stand up, reply"
     replies = Reply.all
     replies_filtered = []
     replies.each do |reply|
       replies_filtered << reply.reply
     end
-    puts "I reply hard, I play hard!"
     render :json => {poo:replies_filtered}
   end
 end
