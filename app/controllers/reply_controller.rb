7f95c2841cceb65c7cf87ecb6451bc183bba7743
kyle smith <chessmonger2112@yahoo.com>
2016-06-02 18:09:31 -0700

    more debugging

--- app/controllers/reply_controller.rb
+++ app/controllers/reply_controller.rb
@@ -2,6 +2,7 @@ class ReplyController < ApplicationController
   def reply
     puts "Shake and Bake!!!"
     puts "the amount of entries in the database are #{Reply.count}"
+    puts "not sure what should be here, but think it should be some sort of active record. #{Reply.class}"
     replies = Reply.all
     replies_filtered = []
     # replies.each do |reply|
