f10b7e864d5f74af748e02f805e677868a9dfec9
kyle smith <chessmonger2112@yahoo.com>
2016-06-02 21:36:01 -0700

    should be able to seed the datebase now

--- app/controllers/reply_controller.rb
+++ app/controllers/reply_controller.rb
@@ -14,7 +14,7 @@ class ReplyController < ApplicationController
 
   def add
     reponses = ["No it's not Space Jam","No it's not The Jam","Still not Space Jam","You wish this was Space Jam","If you think this movie is Space Jam, you clearly don't know Space Jam","This isn't Space Jam, therefore you are stupid","This is not Space Jam. Sorry to hear about that bro."]
-    reponses.length.times do |response|
+    reponses.each do |response|
       new_resonse = Reply.create reply: response
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get "calculate" => "welcome#determine_spacejam"
   get "replies" => "reply#reply"
   get "test" => "welcome#test"
-  get "add" => "replay#add"
+  get "add" => "reply#add"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
