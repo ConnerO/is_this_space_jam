e560123653f9a52d468a171c1796097853504d2b
kyle smith <chessmonger2112@yahoo.com>
2016-06-02 21:27:48 -0700

    attempting to seed the database

--- app/controllers/reply_controller.rb
+++ app/controllers/reply_controller.rb
@@ -11,4 +11,11 @@ class ReplyController < ApplicationController
     render :json => {poo:replies_filtered}
     # render :json => {sup: Reply.count}
   end
+
+  def add
+    reponses = ["No it's not Space Jam","No it's not The Jam","Still not Space Jam","You wish this was Space Jam","If you think this movie is Space Jam, you clearly don't know Space Jam","This isn't Space Jam, therefore you are stupid","This is not Space Jam. Sorry to hear about that bro."]
+    reponses.length.times do |response|
+      new_resonse = Reply.create reply: response
+    end
+  end
 end
