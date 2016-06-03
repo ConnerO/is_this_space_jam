3308dbd0b9d4cb5b4372be288c0de07b7af39ecd
kyle smith <chessmonger2112@yahoo.com>
2016-06-02 21:32:13 -0700

    forgot to update the route file. Did that. Hope it works now

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get "calculate" => "welcome#determine_spacejam"
   get "replies" => "reply#reply"
   get "test" => "welcome#test"
+  get "add" => "replay#add"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
