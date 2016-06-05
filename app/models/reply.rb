b899bbc6f2d477b17c1960a846153ab389074b7a
kyle smith <chessmonger2112@yahoo.com>
2016-06-05 14:13:51 -0700

    took out the fb message from the backend. Api only returns whether or not movie is Space Jam.

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,14 +1,6 @@
 class Reply < ActiveRecord::Base
   def self.calculate movie
-    is_spacejam = false
-    fbmessage = ""
-    if movie == "Space Jam"
-      is_spacejam = true
-      fbmessage = "I was correct! #{movie} is indeed Space Jam. Thank god for 'Is This Space Jam'!"
-    else
-      fbmessage = "I thought #{movie} was Space Jam. Thank god for 'Is This Space Jam'. Now I know it's not.";
-    end
-    {fbmessage: fbmessage,
-    is_spacejam: is_spacejam}
+    is_spacejam = movie == "Space Jam"
+    {is_spacejam: is_spacejam}
   end
 end
